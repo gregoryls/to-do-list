eacc4102834e40b0c56fd2c286b9e181ade071fc
Greg Schneider <greg53x@msn.com>
2023-04-16 17:55:22 -0600

    function that returns wrapped header sidebar and content section elements for page layout

--- src/index.js
+++ src/index.js
@@ -7,4 +7,15 @@ export default function createElementWithId(element, id) {
   return elem;
 }
 
-document.body.appendChild(createElementWithId("div", "contentWrapper"));
+function createPageSections() {
+  const wrapper = createElementWithId("div", "contentWrapper");
+
+  const header = createElementWithId("section", "header");
+  const sidebar = createElementWithId("section", "sidebar");
+  const content = createElementWithId("section", "content");
+
+  wrapper.append(header, sidebar, content);
+
+  return wrapper;
+}
+document.body.appendChild(createPageSections());
