a45e1f36d498d108b8d3c9afc554fa1d32b89887
Greg Schneider <greg53x@msn.com>
2023-04-19 23:04:15 -0600

    remove function generating contentWrapper div, intend to just place into body

--- src/index.js
+++ src/index.js
@@ -4,19 +4,4 @@ import headerCreation from "./header";
 import sidebarCreation from "./sidebar";
 import contentCreation from "./content";
 
-function createPageSections() {
-  const wrapper = createElementWithId("div", "contentWrapper");
-
-  // consider doing this section inside modules
-  // const header = createElementWithId("section", "header");
-  // const sidebar = createElementWithId("section", "sidebar");
-  // const content = createElementWithId("section", "content");
-
-  // wrapper.append(header, sidebar, content);
-
-  return wrapper;
-}
-
-document.body.appendChild(createPageSections());
-
 document.body.append(headerCreation());
