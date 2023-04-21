1a6d789d4d1824b2969512a42fd90ef41b7e48ee
Greg Schneider <greg53x@msn.com>
2023-04-20 22:51:37 -0600

    add an h1 title to header

--- src/header.js
+++ src/header.js
@@ -2,10 +2,16 @@ import createElementWithId from "./elementCreate";
 
 // const contentWrapper = document.getElementById("contentWrapper");
 
-function section() {
-  return createElementWithId("section", "header");
+function headerDOM() {
+  const headerWrap = createElementWithId("section", "header");
+
+  const title = createElementWithId("h1", "title");
+  title.textContent = "To-Do List";
+
+  headerWrap.append(title);
+  return headerWrap;
 }
 
 export default function headerCreation() {
-  return section();
+  return headerDOM();
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./style.css";
 import createElementWithId from "./elementCreate";
 import headerCreation from "./header";
-import sidebarCreation from "./sidebar";
-import contentCreation from "./content";
+// import sidebarCreation from "./sidebar";
+// import contentCreation from "./content";
+
+// document.body.appendChild(createElementWithId("div", "test"));
 
 document.body.append(headerCreation());
