190a61c46bf609b95eb9ab3a94b9b3108b46365d
Greg Schneider <greg53x@msn.com>
2023-04-23 23:10:54 -0600

    create a div for task list labels

--- src/content.js
+++ src/content.js
@@ -1,6 +1,14 @@
 import createElementWithId from "./elementCreate";
 
-function contentDOM() {}
+function contentDOM() {
+  const taskLabels = createElementWithId("div", "labels");
+  const taskName = createElementWithId("p");
+  taskName.textContent = "test";
+
+  taskLabels.append(taskName);
+
+  return taskLabels;
+}
 
 export default function contentCreation() {
   return contentDOM();
