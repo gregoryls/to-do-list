ff6e4d41fab89da71e1eb6364e75cd449332c0eb
Greg Schneider <greg53x@msn.com>
2023-04-24 23:00:37 -0600

    import taskCreate to index and log a test object

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import "./style.css";
 import headerCreation from "./header";
 import sidebarCreation from "./sidebar";
 import contentCreation from "./content";
+import taskCreate from "./task";
 
 // document.body.appendChild(createElementWithId("div", "test"));
 
 document.body.append(headerCreation(), sidebarCreation(), contentCreation());
+
+console.log(taskCreate("test", "test desc", "tomorrow", 1, "n/a"));
