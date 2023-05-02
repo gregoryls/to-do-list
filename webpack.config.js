865f6ce7927ab662a16b9e49054697cc11c898bf
Greg Schneider <greg53x@msn.com>
2023-05-01 23:28:43 -0600

    remove style loader from webpack config (minicssextractplugin covers it)

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
