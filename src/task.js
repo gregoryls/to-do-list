7e45a98d67dfed7c01fe22916cc681c9ff45cfee
Greg Schneider <greg53x@msn.com>
2023-05-07 23:01:51 -0600

    add description, due date, and priority to task DOM generation function

--- src/task.js
+++ src/task.js
@@ -8,9 +8,17 @@ export function generateTaskDOM(taskObject) {
   const allTaskElements = document.getElementById("tasks");
 
   const taskWrapper = createElementWithClass("div", "taskWrapper");
+
   const taskName = createElementWithClass("p", "taskName");
   taskName.textContent = taskObject.name;
-  taskWrapper.append(taskName);
+  const taskDescription = createElementWithClass("p", "taskDescription");
+  taskDescription.textContent = taskObject.description;
+  const taskDueDate = createElementWithClass("p", "taskDueDate");
+  taskDueDate.textContent = taskObject.dueDate;
+  const taskPriority = createElementWithClass("p", "taskPriority");
+  taskPriority.textContent = taskObject.priority;
+
+  taskWrapper.append(taskName, taskDescription, taskDueDate, taskPriority);
 
   allTaskElements.appendChild(taskWrapper);
 }
