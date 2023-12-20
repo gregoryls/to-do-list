09ecb713ecce2ca6a2e7fdb3899b49d1841b8ee9
Greg Schneider <greg53x@msn.com>
2023-12-19 23:26:44 -0700

    remove old show/hide modal function code

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,9 @@
-const modal = document.getElementById("taskModal");
-const overlay = document.getElementById("overlay");
-
 export function removeClass(id, className) {
   const element = document.getElementById(id);
   element.classList.remove(className);
-  // modal.classList.remove("hidden");
-  // overlay.classList.remove("hidden");
 }
 
 export function addClass(id, className) {
   const element = document.getElementById(id);
   element.classList.add(className);
-  // modal.classList.add("hidden");
-  // overlay.classList.add("hidden");
 }
