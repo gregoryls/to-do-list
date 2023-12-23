61e611d6a69cf722d4bdda0d6b4d6e80d82f10bf
Greg Schneider <greg53x@msn.com>
2023-12-22 23:10:12 -0700

    remove unused code fragments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import "./style.css";
 // import createElementWithId from "./elementCreate";
-// import headerCreation from "./header";
-// import { sidebarIcon } from "./sidebar";
-// import contentCreation from "./content";
+
 import * as task from "./task";
 import * as project from "./projects";
 import * as utils from "./utils";
-
-// document.body.appendChild(createElementWithId("div", "test"));
-
-// document.body.append(headerCreation(), sidebarCreation(), contentCreation());
-// sidebarIcon();
 
 // TODO make a notes tab
 // investigate disappearing image problem with sidebar icon
@@ -48,13 +41,6 @@ newTaskButton.addEventListener("click", () => {
   const newTask = document.getElementById("taskModal");
 
   utils.removeClass("hidden", "taskModal", "overlay");
-  // utils.removeClass("overlay", "hidden");
-
-  // if (newTask.style.display === "none" || newTask.style.display === "") {
-  //   newTask.style.display = "block";
-  // } else {
-  //   newTask.style.display = "none";
-  // }
 });
 
 const taskSubmit = document.getElementById("taskSubmit");
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,4 @@
 export function removeClass(className, ...IDs) {
-  // for (const id of IDs) {
-  //   const element = document.getElementById(id);
-  //   element.classList.remove(className);
-  // }
-
   for (let i = 0; i < IDs.length; i += 1) {
     const element = document.getElementById(IDs[i]);
     element.classList.remove(className);
