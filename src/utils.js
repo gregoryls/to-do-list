87777d0548c3df6c4e713dc234af0d8cc2c92084
Greg Schneider <greg53x@msn.com>
2024-01-10 23:57:58 -0700

    fix setting default task date to use local timezone date instead of UTC

--- src/utils.js
+++ src/utils.js
@@ -18,11 +18,16 @@ export function setDateInputToToday() {
 
   const dateTime = new Date();
   console.log(dateTime);
+  const year = dateTime.getFullYear();
+  const month = dateTime.getMonth();
+  const day = dateTime.getDate();
+  const todayy = `${year}-0${month + 1}-${day}`;
 
   // Split at 'T' to remove time info after calendar date;
   const today = dateTime.toISOString().split("T")[0];
   console.log(today);
-  dateInput.value = today;
+  console.log(todayy);
+  dateInput.value = todayy;
 
   // Split at blank character to remove time zone info
   const time = dateTime.toTimeString().split(" ")[0];
